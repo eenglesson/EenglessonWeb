ae2924b8d21bb7ab09439d358946aac7ae71a0f4
eenglesson <eliasenglesson00@gmail.com>
2024-10-09 08:58:40 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/EenglessonWeb/', // Replace <repository-name> with your repository name
   plugins: [react()],
-  build: {
-    outDir: 'build', // Output directory set to 'build'
-  },
 });
